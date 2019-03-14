5345f98ca1a7f153f985b0165335de42c82c24f1
lc151874 <lg.chammas@gmail.com>
2019-03-14 16:14:33 +0100

    6 graphs + changement page

--- src/App.js
+++ src/App.js
@@ -1,45 +1,144 @@
 import React, { Component } from 'react';
-
+import logo from './logo.svg';
 import './App.css';
-import { Button } from 'reactstrap';
-import Graph from './components/Graph';
-import TreeMap from './components/TreeMap';
-import {Container, Col, Row} from 'reactstrap';
-import GraphPie from './components/GraphPie';
+
+import {Button, Container, Row, Col} from 'reactstrap';
+
+import Chart from './components/Chart';
+import Chart2 from './components/Chart2';
+import Chart3 from './components/Chart3';
+
 import CompareLine from './components/CompareLine';
-import Form from './components/Form/Form';
+import Graph from './components/Graph';
+import GraphPie from './components/GraphPie';
+
+import {BrowserRouter as Router, Route, Link} from "react-router-dom";
+
+function Formulaire() {
+  return <h2>Formulaire</h2>;
+}
+
+function Dashboard({ routes }) {
+  return (
+    <div>
+      <h2>Dashboard</h2>
+    //faut le rendre plus responsive
+  <Row className="Font">
+        <Col xl="6">
+          <Chart/>
+        </Col>
+
+        <dl/>
+
+        <Col xl="6">
+          <Chart2/>
+        </Col>
+
+        <dl/>
+
+        <Col xl="6">
+          <Chart3/>
+        </Col>
+
+        <dl/>
+
+        <Col xl="6">
+          <CompareLine/>
+        </Col>
+
+        <dl/>
+
+        <Col xl="6">
+          <Graph/>
+        </Col>
+
+        <dl/>
+
+        <Col xl="6">
+          <GraphPie/>
+        </Col>
+
+        <dl/>
+  </Row>
+
+      {routes.map((route, i) => (
+        <RouteWithSubRoutes key={i} {...route} />
+      ))}
+    </div>
+  );
+}
+
+function Bus() {
+  return <h3>Bus</h3>;
+}
+
+function Cart() {
+  return <h3>Cart</h3>;
+}
+
+////////////////////////////////////////////////////////////
+// then our route config
+const routes = [
+  {
+    path: "/formulaire",
+    component: Formulaire
+  },
+  {
+    path: "/dashboard",
+    component: Dashboard,
+    routes: [
+      {
+        path: "/tacos/bus",
+        component: Bus
+      },
+      {
+        path: "/tacos/cart",
+        component: Cart
+      }
+    ]
+  }
+];
+
+// wrap <Route> and use this everywhere instead, then when
+// sub routes are added to any route it'll work
+function RouteWithSubRoutes(route) {
+  return (
+    <Route
+      path={route.path}
+      render={props => (
+        // pass the sub-routes down to keep nesting
+        <route.component {...props} routes={route.routes} />
+      )}
+    />
+  );
+}
+
+
 
 class App extends Component {
   render() {
-
     return (
+      <div className="App">
+        <header className="App-header">
+  <Router>
+  <div>
+        <ul>
+          <li>
+            <Link to="/dashboard">Dashboard</Link>
+          </li>
+          <li>
+            <Link to="/formulaire">Ajout musical</Link>
+          </li>
+        </ul>
 
-      <Container className="Font">
+        {routes.map((route, i) => (
+          <RouteWithSubRoutes key={i} {...route} />
+        ))}
+  </div>
+  </Router>
+        </header>
+      </div>
 
-        <Row>
-          <Col>
-        <Button color="link" href="./Components/Formulaire.js">link</Button>
-        </Col>
-        </Row>
-         
-         <Row>
-           <Col sm='6'>
-           <Graph/>
-           </Col>
-            <Col sm='6'>
-            <GraphPie/>
-            </Col>
-          </Row>
-          <Row>
-          <Col sm='6'>
-           <Form/>
-            </Col>
-            <Col sm='6'>
-            <CompareLine/>
-            </Col>
-            </Row>
-      </Container>
-      
     );
   }
 }
